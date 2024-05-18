64741758dda17246df098f17ae9e6fe4bc5503dc
astifer <artem_pereverzev_04@bk.ru>
2024-05-18 11:43:41 +0300

    add 2 test for ml toxity

--- test/ml_test.py
+++ test/ml_test.py
@@ -1,5 +1,27 @@
 import pytest 
-from ml.init_classifier import model
+from ml.inference import toxicity_check
+from ml.inference import THRESHOLD
 
+print(f"Threshold: {THRESHOLD}\n")
 
+def test_discrimination():
+    text_toxic_label = {
+        "you are nigger": True,
+        "ты красава": False,
+        "Давить вас надо, сепоров сраных": True
+    }
+
+    for text, label in text_toxic_label:
+        assert toxicity_check(text) == label
+
+def test_badwords():
+    text_toxic_label = {
+        "пошел ты нах": True,
+        "закапывать семена растений глубоко не нужно": False,
+        "епта, такого я не ожидал": True,
+        "неудивил бля": True
+    }
+
+    for text, label in text_toxic_label:
+        assert toxicity_check(text) == label
 
